17bd7c34556dd892fa87af0506e59e9abf308e97
John Doe <johndoe@example.com>
2022-06-28 04:36:46 +0000

    Reserve import-misc

---
+++ src/import-misc.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+import boxen from 'boxen';
+import * as $ from './common.js';
+import { v4 as uuid } from 'uuid';
+
+const {client_old, client_new} = await $.getConnection();
+
+/**
+ * 
+ */
+
+
+/**
+ * End
+ */
+ process.exit(1);
